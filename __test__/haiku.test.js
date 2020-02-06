5800c91a2adfbee276ec84d9ddbc81601bfacd82
Geoff Goetz and Jieun Kang <geoff@>
2020-02-06 13:22:47 -0800

    pass all test

--- __test__/haiku.test.js
+++ __test__/haiku.test.js
@@ -13,6 +13,8 @@ describe ('Haiku', () => {
 
   test('should check that a haiku is 3 lines', () => {
     expect(haikuPoem.lineOne).toEqual("this is a haiku");
+    expect(haikuPoem.lineTwo).toEqual("this is also a haiku");
+    expect(haikuPoem.lineThree).toEqual("this is a haiku");
   });
 
   test('should correctly check syllable of a inputted word', () => {
@@ -21,5 +23,10 @@ describe ('Haiku', () => {
 
   test('should check if any one line has the appropriate number of syllables', () => {
     expect(haikuPoem.checkline("this is a haiku")).toEqual(5);
+  });
+
+  test('should check if user input is a haiku', () => {
+    let a = [haikuPoem.checkline("this is a haiku"), haikuPoem.checkline("this is also a haiku"), haikuPoem.checkline("this is a haiku")]
+    expect(a).toEqual([5, 7, 5])
   });
 });
