722db6ab4548f702a284f79fcdeb934bca7c657c
Geoff Goetz and Jieun Kang <geoff@>
2020-02-06 10:14:36 -0800

    pass the first test for checking constructor

--- __test__/haiku.test.js
+++ __test__/haiku.test.js
@@ -1,1 +1,9 @@
-import "./../src/haiku"
+import { Haiku } from "./../src/haiku"
+
+
+describe ('Haiku', () => {
+  test('should check that a haiku is 3 lines', () => {
+    let haikuPoem = new Haiku("a","b","c");
+    expect(haikuPoem.lineOne).toEqual("a");
+  });
+});
--- src/haiku.js
+++ src/haiku.js
@@ -1,9 +1,41 @@
-class Haiku {
+export class Haiku {
   constructor(lineOne, lineTwo, lineThree) {
     this.lineOne = lineOne,
     this.lineTwo = lineTwo,
     this.lineThree = lineThree
   }
 
-  
+  syllables(word) {
+    word = word.toLowerCase();
+    if(word.length <= 3) { return 1; }
+      return word.replace(/(?:[^laeiouy]es|ed|lle|[^laeiouy]e)$/, '')
+                 .replace(/^y/, '')
+                 .match(/[aeiouy]{1,2}/g).length;
+  }
+
+  checkline(sentence) {
+    let count = 0;
+    let words = sentence.split(" ");
+    
+    words.map(function(val, key){
+      count += syllables(val)
+    });
+    return count;
+  }
+
+  haiku(a, b, c){
+    if(!a || !b || !c){
+      return {result: false, error: "Invalid input. Call haiku(line1, line2, line3)"}
+    }
+    result = [checkLine(a), checkLine(b), checkLine(c)];
+    expected = [5, 7, 5];
+    if(arrayCompare(result, expected)){
+      return true;
+    }
+    return {result: false, syllables:result};
+  }
+
+  arrayCompare(a1, a2){
+    return a1.length==a2.length && a1.every(function(v,i) { return v === a2[i]})
+  }
 };
